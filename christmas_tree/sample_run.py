ea7f90e4526d4a1a88de18ff5bc63200bf7f28b9
Tay Cong Run <congrun@hotmail.com>
2020-12-27 13:54:20 +0800

    Add docstrings and use dots to fill Christmas Tree

--- christmas_tree/sample_run.py
+++ christmas_tree/sample_run.py
@@ -3,7 +3,7 @@ I seem to reserve doing the stupidest things for you...
 A Nerd's Christmas Card for his Nerd lover
 """
 from christmas_utils import (
-    gen_asterisks,
+    gen_triangle_lines,
     overlap_lines,
     gen_tree,
     words_overlay,
@@ -13,15 +13,15 @@ from christmas_utils import (
 
 star = overlap_lines(
     [
-        gen_asterisks(7),
-        gen_asterisks(7, inverted=True),
-    ],
+        gen_triangle_lines(7),
+        gen_triangle_lines(7, inverted=True),
+        ],
     offset=5,
     print_longest=True
 )
-stem = ("*" * 3 for i in range(4))
-base = ("*" * (2*i+20) for i in range(5))
-tree = gen_tree(initial=5, spread=4, parts=6)
+stem = ("." * 3 for i in range(4))
+base = ("." * (2*i+20) for i in range(5))
+tree = gen_tree(initial=6, spread=3, parts=5)
 
 with open("love_letter.byte", "rb") as f:
     love_letter = f.read().decode()
